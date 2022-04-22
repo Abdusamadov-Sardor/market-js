393d23efbd23571d69e976ad0f6a1259cb9d35da
Abdusamadov-Sardor <sardorabdusamatov@750gmail.com>
2022-04-22 15:13:22 +0500

    Change Element

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var elInput = document.querySelector(".input");
 var elOut = document.querySelector(".output");
 var elBtn = document.querySelector(".btn");
+var elBtn1 = document.querySelector(".btn1");
 var newList = document.querySelector(".list");
 var newArr = [];
 
@@ -9,20 +10,20 @@ elBtn.addEventListener("click", function(e){
    e.preventDefault();
  
    var newItem = document.createElement("li");
-    newItem.textContent = elInput.value;
+    
   var elInputVal = elInput.value.trim();
-  newArr.push(elInputVal);
-  elOut.appendChild(newList);
+  newItem.textContent = elInputVal
+  newArr.unshift(elInputVal);
+  newList.append(newArr)
   newList.appendChild(newItem);
- 
+  elOut.appendChild(newList);
+  elOut.textContent = newArr
+  
    console.log(newArr);
- 
+
   elInput.value = "";
  
 })
-
-
-
 
 
 
